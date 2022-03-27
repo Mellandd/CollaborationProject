610c55c4577c8dbd977c74de55182675a0140dfb
Jose Luis Mellina Andreu <45824614+Mellandd@users.noreply.github.com>
2022-03-27 18:49:03 +0200

    Update splitting.py

--- Training/splitting.py
+++ Training/splitting.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from pathlib import Path
 
-df = pd.read_csv('/Users/melland/Universidad/CollaborationProject/caret/caretQuestions.csv')
+#This script splits the dataset into training and test.
+
+df = pd.read_csv('/Users/melland/Universidad/CollaborationProject/caret/QADataset.csv')
 train = df.sample(frac=0.9,random_state=200)
 test = df.drop(train.index)
 train.to_csv(str(Path(__file__).parent) + '/caretTraining.csv', index=False, header=True)
