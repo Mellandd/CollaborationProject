82321b2353eb702d42297282f07efda54f2d3503
Jose Luis Mellina Andreu <45824614+Mellandd@users.noreply.github.com>
2022-03-28 10:29:50 +0200

    Update caretAnswering.py

--- old_Version/caretAnswering.py
+++ old_Version/caretAnswering.py
@@ -8,14 +8,11 @@ from torch.utils.data import DataLoader
 dataset = load_dataset( 'csv', data_files='caretQuestions.csv', split='train')
 questions = [str(i) for i in dataset['Question']]
 answers = [str(i) for i in dataset['Answer']]
-pairs = list(zip(questions, answers))
-scores = [0.9 for i in range(0,len(questions))]
-noise = np.random.normal(0,.1, len(scores)).tolist()
-scores = [a+b for a,b in zip(scores,noise)]
-
+scores = [float(i) for i in dataset['Score']]
+pairs = list(zip(questions, answers, scores))
 
 model = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
-training_data = [InputExample(texts= [a,b], label= 1.0) for (a,b) in pairs]
+training_data = [InputExample(texts= [a,b], label= c) for (a,b,c) in pairs]
 train_dataloader = DataLoader(training_data, shuffle=True, batch_size=16)
 train_loss = losses.CosineSimilarityLoss(model)
 
