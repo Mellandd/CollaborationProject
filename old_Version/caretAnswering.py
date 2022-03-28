8875cf3f1feeef051c38a5693ebf74979186b2e5
Jose Luis Mellina Andreu <45824614+Mellandd@users.noreply.github.com>
2022-03-28 10:31:29 +0200

    Update caretAnswering.py

--- old_Version/caretAnswering.py
+++ old_Version/caretAnswering.py
@@ -13,10 +13,10 @@ pairs = list(zip(questions, answers, scores))
 
 model = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
 training_data = [InputExample(texts= [a,b], label= c) for (a,b,c) in pairs]
-train_dataloader = DataLoader(training_data, shuffle=True, batch_size=16)
+train_dataloader = DataLoader(training_data, shuffle=True, batch_size=8)
 train_loss = losses.CosineSimilarityLoss(model)
 
 evaluator = evaluation.EmbeddingSimilarityEvaluator(questions, answers, scores)
 
-model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=2, warmup_steps=100, 
-evaluator=evaluator, evaluation_steps=100, output_path= "sbertCaret")
+model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=5, warmup_steps=10, 
+evaluator=evaluator, evaluation_steps=10, output_path= "sbertCaret")
